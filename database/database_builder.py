aa6c2f3830e2e2bfdfea74acdcc25700c51ee5da
Justin M <jrmacala@uci.edu>
2023-07-08 22:27:17 -0700

    added checks for non-zero text columns; made set_name in sigil primary key

--- database/database_builder.py
+++ database/database_builder.py
@@ -2,33 +2,34 @@
 import sqlite3
 # images?
 # TODO: table for aether codes
+# TODO: enable PRAGMA foreign_keys = ON; somwhere when creating connection
 def create_tables(connection:sqlite3.Connection):
     # creates the tables to be used in the DB
     connection.execute('''
         CREATE TABLE IF NOT EXISTS modifier(
-            name TEXT NOT NULL,
-            modifier_name TEXT NOT NULL PRIMARY KEY,
+            name TEXT NOT NULL CHECK (length(name) > 0),
+            modifier_name TEXT NOT NULL UNIQUE CHECK (length(modifier_name) > 0) PRIMARY KEY,
             combat_type ENUM('Melee', 'Ranged') NULL,
             gen_zone ENUM('Olympus', 'Nile', 'Shinou', 'Yggdrasil', 'Asterim') NULL,
             dmg_type ENUM('Physical', 'Wind', 'Fire', 'Thunder', 'Shadow', 'Light', 'Ice', 'Water') NULL,
             combat_rsc ENUM('Rage', 'Energy', 'Traces', 'Divine Grace') NULL,
-            sig_functor TEXT NOT NULL,
-            access_key TEXT NOT NULL,
-            normal_atk_name TEXT,
-            skill1_name TEXT,
-            skill2_name TEXT,
-            skill3_name TEXT,
-            ult_skill_name TEXT,
-            dodge_skill_name TEXT
-            rec_sigils_odd TEXT NOT NULL,
-            rec_sigils_even TEXT NOT NULL,
+            sig_functor TEXT NOT NULL CHECK (length(sig_functor) > 0),
+            access_key TEXT NOT NULL CHECK (length(access_key) > 0),
+            normal_atk_name TEXT NOT NULL CHECK (length(normal_atk_name) > 0),
+            skill1_name TEXT NOT NULL CHECK (length(skill1_name) > 0),
+            skill2_name TEXT NOT NULL CHECK (length(skill2_name) > 0),
+            skill3_name TEXT NOT NULL CHECK (length(skill3_name) > 0),
+            ult_skill_name TEXT NOT NULL CHECK (length(ult_skill_name) > 0),
+            dodge_skill_name TEXT NOT NULL CHECK (length(dodge_skill_name) > 0),
+            rec_sigils_odd TEXT NOT NULL CHECK (length(rec_sigils_odd) > 0),
+            rec_sigils_even TEXT NOT NULL CHECK (length(rec_sigils_even) > 0)
         ) STRICT;
         ''')
     connection.commit()
     # TODO: create tables for modifier-to-sigil relation
     connection.execute('''
         CREATE TABLE IF NOT EXISTS sigil(
-            set_name TEXT NOT NULL,
+            set_name TEXT NOT NULL PRIMARY KEY CHECK (length(set_name) > 0),
             set_effects TEXT NOT NULL,
         ) STRICT;
     ''')
