d5c361ee922c46a68cfba50cf12b2a6b80f44699
Justin M <jrmacala@uci.edu>
2023-07-10 17:07:17 -0700

    created table for functors, removed redundant columns from modifier

--- database/database_builder.py
+++ database/database_builder.py
@@ -2,6 +2,7 @@
 import sqlite3
 # images?
 # TODO: table for aether codes
+# TODO: Separate tables to hold Skill Descs + 
 # TODO: enable PRAGMA foreign_keys = ON; somwhere when creating connection
 def create_tables(connection:sqlite3.Connection):
     # creates the tables to be used in the DB
@@ -13,7 +14,6 @@ def create_tables(connection:sqlite3.Connection):
             gen_zone ENUM('Olympus', 'Nile', 'Shinou', 'Yggdrasil', 'Asterim') NULL,
             dmg_type ENUM('Physical', 'Wind', 'Fire', 'Thunder', 'Shadow', 'Light', 'Ice', 'Water') NULL,
             combat_rsc ENUM('Rage', 'Energy', 'Traces', 'Divine Grace') NULL,
-            sig_functor TEXT NOT NULL CHECK (length(sig_functor) > 0),
             access_key TEXT NOT NULL CHECK (length(access_key) > 0),
             normal_atk_name TEXT NOT NULL CHECK (length(normal_atk_name) > 0),
             skill1_name TEXT NOT NULL CHECK (length(skill1_name) > 0),
@@ -21,16 +21,13 @@ def create_tables(connection:sqlite3.Connection):
             skill3_name TEXT NOT NULL CHECK (length(skill3_name) > 0),
             ult_skill_name TEXT NOT NULL CHECK (length(ult_skill_name) > 0),
             dodge_skill_name TEXT NOT NULL CHECK (length(dodge_skill_name) > 0),
-            rec_sigils_odd TEXT NOT NULL CHECK (length(rec_sigils_odd) > 0),
-            rec_sigils_even TEXT NOT NULL CHECK (length(rec_sigils_even) > 0)
         ) STRICT;
         ''')
     connection.commit()
-    # TODO: create tables for modifier-to-sigil relation
     connection.execute('''
         CREATE TABLE IF NOT EXISTS sigil(
             set_name TEXT NOT NULL PRIMARY KEY CHECK (length(set_name) > 0),
-            set_effects TEXT NOT NULL,
+            set_effects TEXT NOT NULL
         ) STRICT;
     ''')
     connection.execute('''
@@ -43,6 +40,21 @@ def create_tables(connection:sqlite3.Connection):
             FOREIGN KEY (set_name) REFERENCES sigil(set_name)
         ) STRICT;
     ''')
+    # each tier has their own access key/functor atk boosts
+    # use modifier_name to get the "name" column
+    connection.execute('''
+        CREATE TABLE IF NOT EXISTS functor(
+            functor_name TEXT NOT NULL CHECK (length(functor_name) > 0),
+            gen_zone ENUM('Olympus', 'Nile', 'Shinou', 'Yggdrasil', 'Asterim') NULL,
+            tier UNSIGNED INTEGER NOT NULL CHECK (tier >=3 AND tier <= 5),
+            functor_power_desc TEXT NOT NULL CHECK (length(functor_power_desc) > 0),
+            functor_lore TEXT NOT NULL CHECK (length(functor_lore) > 0),
+            sig_modifier TEXT NOT NULL UNIQUE CHECK (length(modifier_name) > 0),
+            PRIMARY KEY (functor_name, modifier_name),
+            FOREIGN KEY (sig_modifier) REFERENCES modifier(modifier_name)
+        ) STRICT;
+    ''')
+
 
 
 
