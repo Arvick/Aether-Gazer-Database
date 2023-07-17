d53cd619a3be4ffe7a6da012937c665c8f1ea7b2
Justin M <jrmacala@uci.edu>
2023-07-17 13:50:27 -0700

    created function insert_sigil_modifier

--- database/database_builder.py
+++ database/database_builder.py
@@ -129,8 +129,6 @@ def insert_sigil(connection:sqlite3.Connection) -> int:
     """This function handles inserting of data into the sigil table.
     It will ask for the name of the sigil set, as well as its effects.
     If an error occurs when attempting to insert data, an error code is returned."""
-    '''set_name TEXT NOT NULL PRIMARY KEY CHECK (length(set_name) > 0),
-            set_effects TEXT NOT NULL CHECK (length(set_effects) > 0)'''
     try:
         set_name = input("Enter the name of the sigil set: ").strip()
         set_effects = input("Enter the effects of the sigil set: ").strip()
@@ -145,9 +143,23 @@ def insert_sigil(connection:sqlite3.Connection) -> int:
 
 
 
-def insert_sigil_modifier() -> int:
-    pass
-
+def insert_sigil_modifier(connection:sqlite3.Connection) -> int:
+    """This function handles insertion of data into the sigil_modifier table.
+    It will ask for the modifier, reccomended sigil, and if it's for odd or even slots."""
+    try:
+        modifier_name = input("Enter the name of the modifier (NOT original name): ").strip()
+        set_name = input("Enter the name of the sigil set: ").strip()
+        odd_or_even = selecting_option(('Odd', 'Even'), "Indicate which sigil slots this set is reccomended for on the modifier (Odd = 1, 3, 5 / Even = 2, 4, 6): ")
+        connection.execute(f'''INSERT INTO sigil_modifier
+            ({', '.join(tuple(column for column in locals().keys() if column != 'connection'))}) VALUES ({', '.join(tuple("?" for column in locals().keys() if column != 'connection'))});''',
+            tuple(value for value in locals().values() if isinstance(value, str)))
+        connection.commit()
+        return 0
+    except ValueError:
+        return 1
+    except sqlite3.Error as e:
+        print("An Error occured while inserting into the Database: ", str(e))
+        return 1
 
 def insert_functor() -> int:
     pass
