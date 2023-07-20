608ff8b37d937c671a9d8a91e319c597b25dd12c
Justin M <jrmacala@uci.edu>
2023-07-19 20:52:46 -0700

    Mild Changes
    added README

---
+++ README.md
@@ -0,0 +1,4 @@
+This is intended to be a resource for Aether Gazer, with a database containing infomration on modifiers, functors, and sigils, as well
+as an API to retrive information from it
+
+As of now, it's still in development.
--- database/database_queries.py
+++ database/database_queries.py
@@ -1,1 +1,19 @@
 """This module holds functions that make queries to the database, and returns the results."""
+
+'''modifier: search by:
+1. real name
+2. modifier name
+3. gen zone
+4. meele vs physical
+
+functor: search by
+1. name
+2. gen zone
+3. reccomended modifier
+4. rarity
+
+sigil: search by
+1. name
+2. keywords (something like "give me any sigil sets who have this keyword in their description")
+
+'''
