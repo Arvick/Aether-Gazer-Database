ca1e1fb4d651b0e3772242622a0c9b4ee5de1ac2
Justin M <jrmacala@uci.edu>
2023-07-19 20:43:18 -0700

    created skeleton for api.py

--- api/api.py
+++ api/api.py
@@ -1,2 +1,22 @@
 """This module holds the code for the API that communicates with the database."""
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_restful import Api, Resource
+'''
+search options:
+modifier name
+name
+gen zone
+meeele/ranged
+'''
+
+app = Flask(__name__)
+api = Api(app)
+
+class Test(Resource):
+    def get(self):
+        return {"Data": "shsh"}
+
+api.add_resource(Test, "/Test")
+
+if __name__ == "__main__":
+    app.run(debug=True)
