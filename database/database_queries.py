1ecb00aa34795a4c468cef519de3d6651fc39a92
Justin M <jrmacala@uci.edu>
2023-07-24 21:09:43 -0700

    created skeletons for functions

--- database/database_queries.py
+++ database/database_queries.py
@@ -5,6 +5,9 @@
 2. modifier name
 3. gen zone
 4. meele vs physical
+5. element
+6. resource
+
 
 functor: search by
 1. name
@@ -15,5 +18,34 @@ functor: search by
 sigil: search by
 1. name
 2. keywords (something like "give me any sigil sets who have this keyword in their description")
-
 '''
+from pathlib import Path
+import sqlite3
+from database_builder import create_connection
+
+PATH_TO_DB = Path.cwd() / "aether_gazer.db"
+
+def connection_decorator(func:function):
+    pass
+
+def _mod_search():
+    pass
+
+
+def _functor_search():
+    pass
+
+
+def _sigil_search():
+    pass
+
+
+def interface():
+    connection:sqlite3.Connection = create_connection()
+
+
+
+    connection.close()
+
+if __name__ == "__main__":
+    print(PATH_TO_DB)
