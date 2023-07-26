56fb5dffbea59d0a5a569e9826528edb274dd5c0
Justin M <jrmacala@uci.edu>
2023-07-25 17:01:10 -0700

    changed Modifier to a dataclass, added function give_repr to give fields in dict form

--- database/modifier_class.py
+++ database/modifier_class.py
@@ -1,17 +1,16 @@
 """This module contains the class for Modifiers for keeping track of/returning each one's information."""
+from dataclasses import dataclass, asdict
 
-
+@dataclass(frozen=True, kw_only=True)
 class Modifier:
+    codename: str
+    name: str
+    combat_type: str
+    gen_zone: str
+    dmg_type: str
+    combat_rsc: str
+    access_key: str
 
-    def __init__(self, *, codename:str, name:str, combat_type:str, gen_zone:str,
-                 dmg_type:str, combat_rsc:str, access_key:str) -> None:
-        self._codename = codename
-        self._name = name
-        self._combat_type = combat_type
-        self._gen_zone = gen_zone
-        self._dmg_type = dmg_type
-        self._combat_rsc = combat_rsc
-        self._access_key = access_key
-    
-    
+    def give_repr(self):
+        return asdict(self)
     
--- database/test_modifier_class.py
+++ database/test_modifier_class.py
@@ -1,6 +1,6 @@
 from modifier_class import Modifier as m
 import unittest
-
+from dataclasses import FrozenInstanceError
 
 class test_init(unittest.TestCase):
 
@@ -8,17 +8,37 @@ class test_init(unittest.TestCase):
         temp = m(codename="Rahu", name="Asura", combat_type="Ranged",
                 gen_zone="Asterim", dmg_type="Fire", combat_rsc="Energy",
                 access_key= "Arm of Asura")
-        self.assertEqual(temp._codename, "Rahu")
-        self.assertEqual(temp._name, "Asura")
-        self.assertEqual(temp._combat_type, "Ranged")
-        self.assertEqual(temp._gen_zone, "Asterim")
-        self.assertEqual(temp._dmg_type, "Fire")
-        self.assertEqual(temp._combat_rsc, "Energy")
-        self.assertEqual(temp._access_key, "Arm of Asura")
+        self.assertEqual(temp.codename, "Rahu")
+        self.assertEqual(temp.name, "Asura")
+        self.assertEqual(temp.combat_type, "Ranged")
+        self.assertEqual(temp.gen_zone, "Asterim")
+        self.assertEqual(temp.dmg_type, "Fire")
+        self.assertEqual(temp.combat_rsc, "Energy")
+        self.assertEqual(temp.access_key, "Arm of Asura")
+
 
     def test_positional_args_not_allowed(self):
         with self.assertRaises(TypeError):
             m("Rahu", "Asura", "Ranged", "Asterim", "Fire", "Energy", "Arm of Asura")
+
+
+    def cannot_change_frozen_fields(self):
+        temp = m(codename="Rahu", name="Asura", combat_type="Ranged",
+                gen_zone="Asterim", dmg_type="Fire", combat_rsc="Energy",
+                access_key= "Arm of Asura")
+        with self.assertRaises(FrozenInstanceError):
+            temp.codename = "Shinri"
+    
+class test_give_repr(unittest.TestCase):
+
+    def test_give_repr(self):
+        temp = m(codename="Rahu", name="Asura", combat_type="Ranged",
+                gen_zone="Asterim", dmg_type="Fire", combat_rsc="Energy",
+                access_key= "Arm of Asura")
+        self.assertEqual(temp.give_repr(),
+                        {'codename':"Rahu", 'name':"Asura", 'combat_type':"Ranged",
+                'gen_zone':"Asterim", 'dmg_type':"Fire", 'combat_rsc':"Energy",
+                'access_key': "Arm of Asura"})
 
 
 if __name__ == "__main__":
