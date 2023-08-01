cdd96086986d58ebb967d2336f71cf3072cbe8e4
Justin M <jrmacala@uci.edu>
2023-07-31 20:51:54 -0700

    created test route "views" for frontend

--- frontend/website/__init__.py
+++ frontend/website/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from .views import views
+from .auth import auth
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "flhMy0uJMACIg9fZeh7p"
+    app.register_blueprint(views, url_prefix="/")
     return app
 
+def run():
+    """Runs the frontend."""
+    app = create_app()
+    app.run()
