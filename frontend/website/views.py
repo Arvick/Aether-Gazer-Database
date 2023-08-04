bc2cb61e8fceceaeb5af93c4b99123c6649ce65b
Justin M <jrmacala@uci.edu>
2023-08-03 17:31:12 -0700

    created new route "mod" for handling modifier requests
    created template mod.html for displaying modifier lookup results

--- frontend/website/views.py
+++ frontend/website/views.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
 @views.route("/")
 def home():
     return render_template("base.html")
+
+
+@views.route("/mod")
+def modifiers():
+    data = request.args
+    print(dict(data))
+    return render_template("mod.html")
+
+'''
+dict(data) v
+{'gen_zone': 'Asterim', 'combat_type': 'Ranged', 'dmg_type': 'Fire', 'combat_rsc': 'Energy', 'name': '', 
+'modifier_name': ''}
+'''
